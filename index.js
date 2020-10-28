2cef03d125f9007dc88bc6e09cb3142c64a98375
anthonymjimenez <anthonyjim96@gmail.com>
2020-10-28 09:02:59 -0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
-
+  
   // ADD CODE HERE!
+  let movesContainer = document.querySelector('#moves-container')
+  
+  document.onkeydown = function() {
+    let direction;
+    switch(window.event.keyCode) { 
+      case 37: 
+        //left key
+        direction = 'left'
+        break;
+      case 38: 
+        //up key
+        direction = 'up'
+        break;
+      case 39: 
+      // right key
+        direction = 'right'
+        break;
+      case 40: 
+        // down key7
+        direction = 'down'
+      break;
+  }
+  setMoveLi(direction)
+}
+  function setMoveLi(direction) {
+    if(!direction) return
+    
+    let node = document.createElement("LI"); 
+    node.textContent = direction;
+    node.addEventListener('click', () => node.remove())
+    movesContainer.appendChild(node)
+  }
+
+  moveButton = document.querySelector('#move-button')
+
+  moveButton.addEventListener("click", () => { 
+   const firstLi = movesContainer.querySelector("li")
+    if(firstLi) {
+      const direction = firstLi.textContent.toLowerCase()
+      move(direction)
+      firstLi.remove()
+    }
+  })
+
+
 });
